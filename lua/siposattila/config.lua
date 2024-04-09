93a4f90ab9c315ad9faa8d16309778ac7de0c6ef
Sipos Attila <sattipolo@gmail.com>
2024-04-09 07:30:14 +0200

    disable mouse

--- lua/siposattila/config.lua
+++ lua/siposattila/config.lua
@@ -1,15 +1,17 @@
+vim.opt.mouse = "";
+
 vim.opt.nu = true;
 vim.opt.relativenumber = true;
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop = 4;
+vim.opt.softtabstop = 4;
+vim.opt.shiftwidth = 4;
+vim.opt.expandtab = true;
 
 vim.opt.smartindent = true;
 
 vim.opt.wrap = false;
 
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 8;
 
-vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" });
